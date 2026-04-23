65832791d93b7c3869d64e33da441142b759afc9
FacundoCasanova <casanovafacundoa@outlook.com.ar>
2026-04-23 17:18:58 -0300

    feat: estructura inicial NutriGo - Alimentos Naturales

---
+++ src/main/java/com/nutrigo/PosVentaApplication.java
@@ -0,0 +1,13 @@
+package com.nutrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosVentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrigo/PosVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
